0333267c78e05aa03bf205ba8c0942c118478889
AkiraFukushima <h3.poteto@gmail.com>
2018-01-24 14:52:02 +0900

    Add git status info in prompt

--- prompt.zsh
+++ prompt.zsh
@@ -5,18 +5,75 @@ colors
 
 # VCSの情報を取得するzshの便利関数 vcs_infoを使う
 autoload -Uz vcs_info
+autoload -Uz add-zsh-hook
 
 # 表示フォーマットの指定
 # %b ブランチ情報
 # %a アクション名(mergeなど)
+zstyle ':vcs_info:*' max-exports 3
 zstyle ':vcs_info:*' enable git svn
-zstyle ':vcs_info:git:*' check-for-changes true
-zstyle ':vcs_info:git:*' stagedstr "%F{yellow}!"
-zstyle ':vcs_info:git:*' unstagedstr "%F{red}+"
-zstyle ':vcs_info:*' formats "%F{magenta}%c%u[%b]%f"
+zstyle ':vcs_info:*' formats "[%b]"
 zstyle ':vcs_info:*' actionformats '[%b|%a]'
-precmd () { vcs_info }
-RPROMPT=$RPROMPT'${vcs_info_msg_0_}'
+zstyle ':vcs_info:git:*' check-for-changes true
+zstyle ':vcs_info:git:*' stagedstr "!"
+zstyle ':vcs_info:git:*' unstagedstr "+"
+zstyle ':vcs_info:git:*' formats '[%b]' '%c%u'
+zstyle ':vcs_info:git:*' actionformats '[%b|%a]' '%c%u'
+
+
+zstyle ':vcs_info:git+set-message:*' hooks \
+       git-hook-begin \
+       git-untracked
+
+function +vi-git-hook-begin() {
+    if [[ $(command git rev-parse --is-inside-work-tree 2> /dev/null) != 'true' ]]; then
+        # 0以外を返すとそれ以降のフック関数は呼び出されない
+        return 1
+    fi
+
+    return 0
+}
+
+function +vi-git-untracked() {
+    # zstyle formats, actionformats の2番目のメッセージのみ対象にする
+    if [[ "$1" != "1" ]]; then
+        return 0
+    fi
+
+    if command git status --porcelain 2> /dev/null \
+            | awk '{print $1}' \
+            | command grep -F '??' > /dev/null 2>&1 ; then
+
+        # unstaged (%u) に追加
+        hook_com[unstaged]+='?'
+    fi
+}
+
+function _update_vcs_info_msg() {
+    local -a messages
+    local prompt
+
+    LANG=en_US.UTF-8 vcs_info
+
+    if [[ -z ${vcs_info_msg_0_} ]]; then
+        # vcs_info で何も取得していない場合はプロンプトを表示しない
+        prompt=""
+    else
+        # vcs_info で情報を取得した場合
+        # $vcs_info_msg_0_ , $vcs_info_msg_1_ , $vcs_info_msg_2_ を
+        # それぞれ緑、黄色、赤で表示する
+        [[ -n "$vcs_info_msg_0_" ]] && messages+=( "%F{magenta}${vcs_info_msg_0_}%f" )
+        [[ -n "$vcs_info_msg_1_" ]] && messages+=( "%F{yellow}${vcs_info_msg_1_}%f" )
+        [[ -n "$vcs_info_msg_2_" ]] && messages+=( "%F{red}${vcs_info_msg_2_}%f" )
+
+        # 間にスペースを入れて連結する
+        prompt="${(j: :)messages}"
+    fi
+
+    RPROMPT="$prompt"
+}
+
+add-zsh-hook precmd _update_vcs_info_msg
 
 PROMPT="[%n]:%{${fg[cyan]}%}%~%{${reset_color}%}$ "
 
