5ac4ffb49511c44fe53d23d5a9205571333522d4
AkiraFukushima <h3.poteto@gmail.com>
2018-07-30 15:52:39 +0900

    Change prompt two lines

--- prompt.zsh
+++ prompt.zsh
@@ -79,6 +79,7 @@ function _update_vcs_info_msg() {
 add-zsh-hook precmd _update_vcs_info_msg
 
 # https://qiita.com/butaosuinu/items/770a040bc9cfe22c71f4
-PROMPT=$'%{\e[38;5;111m%}[%n]%{\e[0m%}:%{${fg[cyan]}%}%~%{${reset_color}%}$ '
+PROMPT=$'%{\e[38;5;111m%}[%n]%{\e[0m%}:%{${fg[cyan]}%}%~%{${reset_color}%}
+$ '
 
 PROMPT2='[%n]> '
